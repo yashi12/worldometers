38cf210b8108512c7a058579e76b5626b4230de9
yashi12 <yashiagarwal1812@gmail.com>
2020-10-18 16:01:21 +0530

    Pass data between 2 functions
    - Add meta tag
    - Use response to fetch value

--- worldometers/spiders/countries.py
+++ worldometers/spiders/countries.py
@@ -14,15 +14,17 @@ class CountriesSpider(scrapy.Spider):
             name = country.xpath(".//text()").get()
             link = country.xpath(".//@href").get()
 
-            yield response.follow(url=link, callback=self.parse_country)
+            yield response.follow(url=link, callback=self.parse_country, meta={'country_name':name})
 
     def parse_country(self, response):
+        name = response.request.meta['country_name']
         rows = response.xpath("(//table[@class='table table-striped table-bordered table-hover table-condensed table-list'])[1]/tbody/tr")
         for row in rows:
             year = row.xpath(".//td[1]/text()").get()
             population = row.xpath(".//td[2]/strong/text()").get()
 
             yield{
+                'country_name' : name,
                 'year': year,
                 'population': population
             }
