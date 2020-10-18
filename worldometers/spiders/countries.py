21c5a668a578dc17af8c0fc9b575751f52d93860
yashi12 <yashiagarwal1812@gmail.com>
2020-10-18 18:55:18 +0530

    Change User-Agent
    - Change user agent from scrapy to Mozilla

--- worldometers/spiders/countries.py
+++ worldometers/spiders/countries.py
@@ -7,6 +7,11 @@ class CountriesSpider(scrapy.Spider):
     allowed_domains = ['www.worldometers.info']
     start_urls = ['https://www.worldometers.info/world-population/population-by-country/']
 
+    def start_requests(self):
+        yield scrapy.Request(url='https://www.worldometers.info/world-population/population-by-country/',callback=self.parse, headers={
+            'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36'
+        })
+
     def parse(self, response):
         countries = response.xpath("//td/a")
 
@@ -14,10 +19,11 @@ class CountriesSpider(scrapy.Spider):
             name = country.xpath(".//text()").get()
             link = country.xpath(".//@href").get()
 
-            yield response.follow(url=link, callback=self.parse_country, meta={'country_name':name})
+            yield response.follow(url=link, callback=self.parse_country, meta={'country_name':name, 'user-agent': response.request.headers['User-Agent']})
 
     def parse_country(self, response):
         name = response.request.meta['country_name']
+        user_agent = response.request.meta['user-agent']
         rows = response.xpath("(//table[@class='table table-striped table-bordered table-hover table-condensed table-list'])[1]/tbody/tr")
         for row in rows:
             year = row.xpath(".//td[1]/text()").get()
@@ -26,5 +32,6 @@ class CountriesSpider(scrapy.Spider):
             yield{
                 'country_name' : name,
                 'year': year,
-                'population': population
+                'population': population,
+                'user-agent': user_agent
             }
