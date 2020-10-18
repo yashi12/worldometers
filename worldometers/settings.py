faffebf6f2c94d734254018aaf98e58591ba6260
yashi12 <yashiagarwal1812@gmail.com>
2020-10-18 19:02:30 +0530

    Change user agent for all responses
    - Change Default_Request_Header in settings.py

--- worldometers/settings.py
+++ worldometers/settings.py
@@ -41,6 +41,9 @@ ROBOTSTXT_OBEY = True
 #   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
 #   'Accept-Language': 'en',
 #}
+DEFAULT_REQUEST_HEADERS = {
+  'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36'
+}
 
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
--- worldometers/spiders/countries.py
+++ worldometers/spiders/countries.py
@@ -7,10 +7,10 @@ class CountriesSpider(scrapy.Spider):
     allowed_domains = ['www.worldometers.info']
     start_urls = ['https://www.worldometers.info/world-population/population-by-country/']
 
-    def start_requests(self):
-        yield scrapy.Request(url='https://www.worldometers.info/world-population/population-by-country/',callback=self.parse, headers={
-            'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36'
-        })
+    # def start_requests(self):
+    #     yield scrapy.Request(url='https://www.worldometers.info/world-population/population-by-country/',callback=self.parse, headers={
+    #         'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36'
+    #     })
 
     def parse(self, response):
         countries = response.xpath("//td/a")
@@ -19,11 +19,12 @@ class CountriesSpider(scrapy.Spider):
             name = country.xpath(".//text()").get()
             link = country.xpath(".//@href").get()
 
-            yield response.follow(url=link, callback=self.parse_country, meta={'country_name':name, 'user-agent': response.request.headers['User-Agent']})
+            yield response.follow(url=link, callback=self.parse_country, meta={'country_name':name,'user-agent':response.request.headers['User-Agent']})
 
     def parse_country(self, response):
         name = response.request.meta['country_name']
         user_agent = response.request.meta['user-agent']
+        # user_agent = response.request.headers['User-Agent']
         rows = response.xpath("(//table[@class='table table-striped table-bordered table-hover table-condensed table-list'])[1]/tbody/tr")
         for row in rows:
             year = row.xpath(".//td[1]/text()").get()
